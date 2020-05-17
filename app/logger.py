1a3369afbc34e435ee9c3184142b647b891f0828
leafwind <connor.hsu@smartnews.com>
2020-05-18 00:43:22 +0900

    add a comment to logger

--- app/logger.py
+++ app/logger.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 
 def set_logger(log_level=20):
+    # TODO: consider put "%(filename)s" in the custom format
     my_format = "[%(levelname).4s] %(asctime)s | %(name)s | " \
                 "%(lineno)3s | %(message)s"
     date_format = '%Y-%m-%d %H:%M:%S'
